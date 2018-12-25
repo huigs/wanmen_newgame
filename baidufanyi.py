28500b8e8258263a4717038ad7129eff31c2bfe0
huigs <huigs@tom.com>
2018-12-25 17:24:16 +0800

    add baidufanyi

---
+++ baidufanyi.py
@@ -0,0 +1,39 @@
+import requests
+import json
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36"}
+# 2个请求的URL地址
+langdetect_url = "http://fanyi.baidu.com/langdetect"
+transapi_url = "http://fanyi.baidu.com/v2transapi"
+
+while True:
+    input_word = input("Translate input: ")
+
+    langdetect_data = {"query": input_word}
+
+    langdetect_resp = requests.post(langdetect_url, headers=headers, data=langdetect_data)
+    langdetect_dict = json.loads(langdetect_resp.content.decode())
+    print(langdetect_dict)
+    langdetect = langdetect_dict["lan"]
+
+    if langdetect == "en":  # 检测到输入为英文，翻译成中文
+        langtransto = "zh"
+    else:
+        langtransto = "en"  # 输入为中文则翻译成英文
+
+    transapi_data = {"from": langdetect,
+                     "to": langtransto,
+                     "query": input_word,
+                     "transtype": "translang",
+                     "simple_means_flag": 3,
+                     "sign": 484403.231170,
+                     "token": "9b9736e837ab4fd40c9b0ee85b7fa213"}
+    # sign和token不同的用户可能不一样
+
+    transapi_reap = requests.post(transapi_url, headers=headers, data=transapi_data)
+
+    transapi_dict = json.loads(transapi_reap.content.decode())
+    # json.loads()函数可以将网页的响应转化为字典格式
+    # （如果响应的格式是像字典一样的字符串，即json字符串，的话）
+    trans_str = transapi_dict["trans_result"]["data"][0]["dst"]
--- translate.py
+++ translate.py
@@ -6,18 +6,18 @@ import pprint
 import time
 
 appid = '12345678'
-key  = 'dasd457dawgjj54j01qf'
+key = 'dasd457dawgjj54j01qf'
  
 url = 'https://fanyi.baidu.com/pcnewcollection?'
-#需要翻译的文本
+# 需要翻译的文本
 q = '建设中国特色社会主义'
-#原语言
-from_language ='zh'
-#目的语言
+# 原语言
+from_language = 'zh'
+# 目的语言
 to_language = 'en'
-#随机数
+# 随机数
 salt = random.randint(32768, 65536)
-#签名
+# 签名
 sign = appid+q+str(salt)+key
 sign = sign.encode('utf-8')
 sign_new = hashlib.md5(sign).hexdigest()
@@ -26,14 +26,14 @@ prefix='https://fanyi.baidu.com/pcnewcollection?req=check&fanyi_src=%E4%B8%AD%E5
 
 t = time.time()
 nowTime = lambda:int(round(t * 1000))
-print (nowTime());              #毫秒级时间戳，基于lambda
-#生成URL
-#new_url = url + 'q='+q+'&from='+from_language+'&to='+to_language+'&appid='+appid+'&salt='+str(salt)+'&sign='+sign_new
-#new_url = 'https://fanyi.baidu.com/pcnewcollection?req=check&fanyi_src=%E4%B8%AD%E5%9B%BD&direction=zh2en&_=1545638156767'
+print(nowTime())              # 毫秒级时间戳，基于lambda
+# 生成URL
+# new_url = url + 'q='+q+'&from='+from_language+'&to='+to_language+'&appid='+appid+'&salt='+str(salt)+'&sign='+sign_new
+# new_url = 'https://fanyi.baidu.com/pcnewcollection?req=check&fanyi_src=%E4%B8%AD%E5%9B%BD&direction=zh2en&_=1545638156767'
 new_url = prefix+str(nowTime())
 
 res = requests.get(new_url)
 print(res.text)
-#json_data = json.loads(res.text)
-#translate_result = json_data["trans_result"]["dst"]
-#pprint.pprint(json_data["trans_result"])
+# json_data = json.loads(res.text)
+# translate_result = json_data["trans_result"]["dst"]
+# pprint.pprint(json_data["trans_result"])
